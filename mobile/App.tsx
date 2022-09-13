dba594eeca6407894b64932294243b2c0d32543f
Alexandre Bulcao <ambulcao@gmail.com>
2022-09-13 08:53:11 +0100

    Creating project mobile
    Creating project mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,46 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+          eSport to the Game
+      </Text>
+      <Button title="Call of Duty"/>
+      <Button title="CSGO"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.button}>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 28
+  },
+  button: {
+    backgroundColor: '#fff',
+    marginTop: 20,
+  }
+});
