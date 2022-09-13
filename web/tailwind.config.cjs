9282b998b7afba24f90aa60cb30d757640188896
Alexandre Bulcao <ambulcao@gmail.com>
2022-09-13 15:30:01 +0100

    Creating langPage
    Creating LangPage project web

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,9 +2,19 @@
 module.exports = {
   content: [
     './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(90.96deg, #9572FC 15.08%, #43E7AD 85.84%, #E1D55D 1.47%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.88%)',
+      },
+    },
   },
   plugins: [],
 }
