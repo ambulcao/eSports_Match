49594b2d56a61ad5ff04b8af3a428dc4811832e3
Alexandre Bulcao <ambulcao@gmail.com>
2022-09-27 17:04:46 +0100

    Changed fetch with axios
    Changed fetch with axios in the project

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import  axios  from 'axios';
 import * as Dialog from '@radix-ui/react-dialog'
 import { CreateAdBanner } from './components/CreateAdBanner';
 
@@ -23,10 +24,8 @@ function App() {
     const [games, setGames] = useState<Game[]>([]);
   
     useEffect(() => {
-        fetch('http://localhost:3333/games')
-            .then(response => response.json())
-            .then(data => {
-                setGames(data);
+        axios('http://localhost:3333/games').then(response => {
+            setGames(response.data);
             })
     }, [])
 
